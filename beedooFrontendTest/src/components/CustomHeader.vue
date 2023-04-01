df7daeee506167e165b3e2aa1bd406eed9fc00e6
Raidro <mateus_sat@hotmail.com>
2023-04-01 17:21:14 -0300

    criação do menu adicionando a img do beedoo (WIP)

--- beedooFrontendTest/src/components/CustomHeader.vue
+++ beedooFrontendTest/src/components/CustomHeader.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>header ok</h1>
+  <v-container>
+    <v-row>
+      <v-col class="bg-deep-purple">
+        <v-img src="../../public/logo-beedoo.png" alt="logo" width="200" height="50"></v-img>
+      </v-col>
+      <v-col class="bg-black"> </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup></script>
