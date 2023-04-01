827ddc39fe6d7554dc1165928b3eb1055ddd42ca
Raidro <mateus_sat@hotmail.com>
2023-04-01 17:53:38 -0300

    adiicona os 2 button round

--- beedooFrontendTest/src/components/CustomHeader.vue
+++ beedooFrontendTest/src/components/CustomHeader.vue
@@ -1,13 +1,17 @@
 <template>
   <v-container>
     <v-row>
-      <v-col class="bg-deep-purple">
-        <v-img src="../../public/logo-beedoo.png" alt="logo" width="200" height="50"></v-img>
+      <v-col class="ma-2">
+        <v-img src="../../public/logo-beedoo.png" alt="logo-beedoo" width="200" height="50"></v-img>
       </v-col>
-      <v-col class="bg-black">
-        <v-btn class="ma-2" color="indigo" icon="mdi-home"></v-btn>
+
+      <v-col class="d-flex flex-row-reverse">
+        <v-btn class="ma-2" color="grey-lighten-2" icon="mdi-account-circle"></v-btn>
+        <v-btn class="ma-2" color="grey-lighten-2" icon="mdi-cart"></v-btn>
       </v-col>
     </v-row>
+
+    <v-row> </v-row>
   </v-container>
 </template>
 
