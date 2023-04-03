b070376d54ad7a87692471e8e0284de4696b01aa
Raidro <mateus_sat@hotmail.com>
2023-04-03 14:45:41 -0300

    move entre os compoenntes

--- beedooFrontendTest/src/components/CardTestes.vue
+++ beedooFrontendTest/src/components/CardTestes.vue
@@ -1,5 +1,23 @@
 <template>
-  <v-card class="ml-4 mr-4 mt-1" :title="title" />
+  <v-card class="ml-4 mr-4 mt-1">
+    <v-row>
+      <v-col class="mt-2">
+        <v-text class="ml-4 text-subtitle-2 font-weight-bold">{{ title }}</v-text>
+      </v-col>
+      <v-col cols="3">
+        <div class="d-flex justify-end align-self-start">
+          <v-btn
+            @click="$router.push('testes')"
+            class="ma-2"
+            size="large"
+            variant="text"
+            icon="mdi-chevron-right"
+            color="grey"
+          />
+        </div>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
 <script setup>
 import { defineProps } from 'vue'
---
+++ beedooFrontendTest/src/views/TestsViews.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>OKOKOK</div>
+</template>
+<script setup></script>
+<style scoped></style>
