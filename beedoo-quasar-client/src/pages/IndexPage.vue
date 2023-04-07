c4ab1119c438ea8fbaa47dc64bebacfd39081b38
Lucas Marra <marralucas1@gmail.com>
2023-04-07 11:15:01 -0300

    Starting project, mobile q-header is out of the box

--- beedoo-quasar-client/src/pages/IndexPage.vue
+++ beedoo-quasar-client/src/pages/IndexPage.vue
@@ -1,10 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <h1>Beedoo Edtech Quiz</h1>
   </q-page>
 </template>
 
